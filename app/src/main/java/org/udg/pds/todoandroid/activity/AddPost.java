89de2a3fefec69e9f66e970d86f3c05cc33c53ef
Oriol <ori_tossa_98@hotmail.com>
2019-04-16 13:16:09 +0200

    Create AddPost activity and interface

---
+++ app/src/main/java/org/udg/pds/todoandroid/activity/AddPost.java
@@ -0,0 +1,14 @@
+package org.udg.pds.todoandroid.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.udg.pds.todoandroid.R;
+
+public class AddPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_post);
+    }
+}
