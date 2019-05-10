4e6120a418e71337ce43d9199c6538d8e73547bf
atp94 <u1923163@campus.udg.edu>
2019-05-10 20:41:33 +0200

    Change TextView for a Button in Post Page to go to Creator's profile

--- app/src/main/java/org/udg/pds/todoandroid/activity/OtherUserProfile.java
+++ app/src/main/java/org/udg/pds/todoandroid/activity/OtherUserProfile.java
@@ -1,4 +1,217 @@
 package org.udg.pds.todoandroid.activity;
 
-public class OtherUserProfile {
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import org.udg.pds.todoandroid.R;
+import org.udg.pds.todoandroid.TodoApp;
+import org.udg.pds.todoandroid.entity.Game;
+import org.udg.pds.todoandroid.entity.User;
+import org.udg.pds.todoandroid.rest.TodoApi;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public class OtherUserProfile extends AppCompatActivity {
+
+    TodoApi mTodoService;
+    View rootView;
+    RecyclerView mRecyclerView;
+    private TRAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.other_user_profile);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mTodoService = ((TodoApp) this.getApplication()).getAPI();
+
+        getUserInfo();
+
+        mRecyclerView = findViewById(R.id.subscribed_games);
+        mAdapter = new OtherUserProfile.TRAdapter(this.getApplication());
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getUserInfo();
+    }
+
+    public void getUserInfo(){
+        Call<User> call = mTodoService.getMe();
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if(response.isSuccessful()){
+                    showProfileUserInfo(response.body());
+                    OtherUserProfile.this.showGameList(response.body().games);
+                } else {
+                    Toast.makeText(OtherUserProfile.this.getBaseContext(), "Error reading user", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+            }
+        });
+    }
+
+    public void showProfileUserInfo(User user){
+
+        TextView userUsername;
+        TextView userDescription;
+        RatingBar userRating;
+
+        userUsername = rootView.findViewById(R.id.user_username);
+        userDescription = rootView.findViewById(R.id.user_description);
+        userRating = rootView.findViewById(R.id.user_rating);
+
+        userUsername.setText(user.name);
+        userDescription.setText(user.description);
+        userRating.setRating(user.valoration);
+        new OtherUserProfile.DownloadImageFromInternet((ImageView) rootView.findViewById(R.id.user_image)).execute(user.image);
+    }
+
+    public void showGameList(List<Game> tl) {
+        mAdapter.clear();
+        for (Game t : tl) {
+            mAdapter.add(t);
+        }
+    }
+
+    static class GameViewHolder extends RecyclerView.ViewHolder {
+        TextView categories;
+        TextView title;
+        ImageView logo;
+        View view;
+
+        GameViewHolder(View itemView) {
+            super(itemView);
+            view = itemView;
+            categories = (TextView) itemView.findViewById(R.id.ListGameCategories);
+            title = (TextView) itemView.findViewById(R.id.ListGameTitle);
+            logo =  (ImageView) itemView.findViewById(R.id.listGameLogo);
+        }
+    }
+
+    class TRAdapter extends RecyclerView.Adapter<OtherUserProfile.GameViewHolder> {
+
+        List<Game> list = new ArrayList<>();
+        Context context;
+
+        public TRAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public OtherUserProfile.GameViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.game_layout, parent, false);
+            OtherUserProfile.GameViewHolder holder = new OtherUserProfile.GameViewHolder(v);
+
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(OtherUserProfile.GameViewHolder holder, final int position) {
+            holder.categories.setText(list.get(position).categories.toString());
+            holder.title.setText(list.get(position).name);
+            new OtherUserProfile.DownloadImageFromInternet((ImageView)holder.logo).execute(list.get(position).image);
+
+            holder.view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent i = new Intent(OtherUserProfile.this, GameProfile.class);
+                    i.putExtra("gameId",list.get(position).id);
+                    startActivity(i);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+
+            super.onAttachedToRecyclerView(recyclerView);
+        }
+
+        // Insert a new item to the RecyclerView
+        public void insert(int position, Game data) {
+            list.add(position, data);
+            notifyItemInserted(position);
+        }
+
+        // Remove a RecyclerView item containing the Data object
+        public void remove(Game data) {
+            int position = list.indexOf(data);
+            list.remove(position);
+            notifyItemRemoved(position);
+        }
+
+        public void add(Game t) {
+            list.add(t);
+            this.notifyItemInserted(list.size() - 1);
+        }
+
+        public void clear() {
+            int size = list.size();
+            list.clear();
+            this.notifyItemRangeRemoved(0, size);
+        }
+    }
+
+
+    private class DownloadImageFromInternet extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+
+        public DownloadImageFromInternet(ImageView imageView) {
+            this.imageView = imageView;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String imageURL = urls[0];
+            Bitmap bimage = null;
+            try {
+                InputStream in = new java.net.URL(imageURL).openStream();
+                bimage = BitmapFactory.decodeStream(in);
+
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Error loading image", Toast.LENGTH_SHORT).show();
+            }
+            return bimage;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
+    }
+
+
 }
