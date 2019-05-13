780c7609faeddb18434c398d848d6f1cbcd35e8a
atp94 <u1923163@campus.udg.edu>
2019-05-13 22:58:28 +0200

    Modified TextView on Profile Settings

--- app/src/main/java/org/udg/pds/todoandroid/activity/ProfileSettings.java
+++ app/src/main/java/org/udg/pds/todoandroid/activity/ProfileSettings.java
@@ -2,6 +2,9 @@ package org.udg.pds.todoandroid.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.widget.EditText;
+
 import org.udg.pds.todoandroid.R;
 import org.udg.pds.todoandroid.rest.TodoApi;
 
@@ -13,6 +16,9 @@ public class ProfileSettings extends AppCompatActivity {
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_settings);
+
+        EditText description = (EditText) findViewById(R.id.settings_description);
+        description.setRawInputType(InputType.TYPE_CLASS_TEXT);
     }
 
 }
