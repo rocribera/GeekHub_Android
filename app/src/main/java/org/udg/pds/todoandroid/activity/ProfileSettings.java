f80547c17c3c8608b9c261afa8658836aba463e3
atp94 <u1923163@campus.udg.edu>
2019-05-14 02:31:04 +0200

    Reload settings image when loses link's edittext focus

--- app/src/main/java/org/udg/pds/todoandroid/activity/ProfileSettings.java
+++ app/src/main/java/org/udg/pds/todoandroid/activity/ProfileSettings.java
@@ -1,14 +1,22 @@
 package org.udg.pds.todoandroid.activity;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
 import android.text.InputType;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.udg.pds.todoandroid.R;
@@ -39,9 +47,42 @@ public class ProfileSettings extends AppCompatActivity {
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_settings);
+        mTodoService = ((TodoApp) this.getApplication()).getAPI();
 
         EditText description = (EditText) findViewById(R.id.settings_description);
         description.setRawInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+
+        // To send the new settings to server
+        Button uploadButton = (Button) findViewById(R.id.update_button);
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        // To clear focus when last input has been done and update the image if link has text
+        EditText linkText = findViewById(R.id.settings_link);
+        linkText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId== EditorInfo.IME_ACTION_DONE) {
+                    // To clear focus, probably exists a better way for just remove the cursor
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(),0);
+                    ConstraintLayout cl =findViewById(R.id.focusable_settings_layout);
+                    cl.requestFocus();
+                    // To update the image
+                    EditText linkImage = findViewById(R.id.settings_link);
+                    String link = linkImage.getText().toString();
+                    if (!link.isEmpty())
+                        new ProfileSettings.DownloadImageFromInternet((ImageView) findViewById(R.id.settings_image)).execute(link);
+                    else
+                        new ProfileSettings.DownloadImageFromInternet((ImageView) findViewById(R.id.settings_image)).execute(originalImageLink);
+
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -58,8 +99,7 @@ public class ProfileSettings extends AppCompatActivity {
         getOriginalUserInfo();
     }
 
-    public void getOriginalUserInfo()
-    {
+    public void getOriginalUserInfo() {
         Call<User> call = mTodoService.getMe();
         call.enqueue(new Callback<User>() {
             @Override
@@ -78,8 +118,7 @@ public class ProfileSettings extends AppCompatActivity {
         });
     }
 
-    public void showOriginalUserInfo(User u)
-    {
+    public void showOriginalUserInfo(User u) {
         EditText username = (EditText) this.findViewById(R.id.settings_username);
         EditText description = (EditText) this.findViewById(R.id.settings_description);
         EditText linkImage = (EditText) this.findViewById(R.id.settings_link);
@@ -117,6 +156,5 @@ public class ProfileSettings extends AppCompatActivity {
             imageView.setImageBitmap(result);
         }
     }
-
 
 }
